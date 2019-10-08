13b59569057ced782f3a6a6d30096c29ffae1046
Zuxriddin Kamalov <bigjoss@mail.ru>
2019-10-08 14:28:46 +0500

    Server port fix

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 const express = require('express')
 const serveStatic = require('serve-static')
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 8000
 const app = express()
 
 app.use(serveStatic(__dirname + '/dist/'))
